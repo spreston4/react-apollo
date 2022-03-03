34e1e3bd5eb537e7abb7543abaf6dc8acafd8209
Sam Preston <sam.preston11@gmail.com>
2022-03-03 15:34:53 -0600

    Finished Time up for assessment window.

--- src/Components/LinkDisplay/LinkDisplay.js
+++ src/Components/LinkDisplay/LinkDisplay.js
@@ -1,17 +1,23 @@
 import styles from "./LinkDisplay.module.css";
+import { useEffect, useState } from "react";
 import { useQuery } from "@apollo/client";
 import { GET_ALL_LINKS } from "../../GraphQL/queries";
 
-// This function is very tempermental, If the backend sits untouched for a few minutes it stops responding, then this component stops working.
+// This function is very tempermental, if the backend sits untouched for a few minutes it stops responding, then this component stops working.
 const LinkDisplay = () => {
   const { loading, error, data } = useQuery(GET_ALL_LINKS);
+  const [links, setLinks] = useState([]);
 
-  // console.log(data);
-  // console.log(data.getAllLinks);
+  // Not sure why, but accessing the 'getAllLinks' object returned with data works for about 5 minutes at a time, then starts throwing errors when unrelated parts of code are touched.
+  useEffect(() => {
+    // setLinks(data.getAllLinks);
+  }, [data]);
+
+  console.log(links);
 
   return (
     <div className={styles.container}>
-      {data.getAllLinks.map((link) => (
+      {links.map((link) => (
         <p key={link.id}>
           {link.url} {"   =>   "} {link.slug}
         </p>
--- src/index.js
+++ src/index.js
@@ -2,13 +2,7 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  useQuery,
-  gql
-} from "@apollo/client";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
   uri: "https://71hds9.sse.codesandbox.io/",
