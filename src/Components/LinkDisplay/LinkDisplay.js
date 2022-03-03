5f006e468978d77b1066f249b17d485eb3871cbb
Sam Preston <sam.preston11@gmail.com>
2022-03-03 13:05:18 -0600

    LinkDisplay component created Queries getAllLinks & displays link & slug to the page

---
+++ src/Components/LinkDisplay/LinkDisplay.js
@@ -0,0 +1,20 @@
+import { useQuery } from "@apollo/client";
+import { GET_ALL_LINKS } from "../../GraphQL/queries";
+
+const LinkDisplay = () => {
+  const { loading, error, data } = useQuery(GET_ALL_LINKS);
+
+  console.log(data.getAllLinks);
+
+  return (
+    <div>
+      {data.getAllLinks.map((link) => (
+        <p key={link.id}>
+          {link.url} {"=>"} {link.slug}{" "}
+        </p>
+      ))}
+    </div>
+  );
+};
+
+export default LinkDisplay;
