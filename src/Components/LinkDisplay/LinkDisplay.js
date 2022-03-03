1414ab07976bab2efbadd21d2555b92298c8b8b0
Sam Preston <sam.preston11@gmail.com>
2022-03-03 15:16:05 -0600

    LinkDsiplay formatting formating on LinkDisplay component

--- src/Components/LinkDisplay/LinkDisplay.js
+++ src/Components/LinkDisplay/LinkDisplay.js
@@ -1,20 +1,21 @@
+import styles from "./LinkDisplay.module.css";
 import { useQuery } from "@apollo/client";
 import { GET_ALL_LINKS } from "../../GraphQL/queries";
 
-// This stopped working halfway through. Not sure why.
+// This function is very tempermental, If the backend sits untouched for a few minutes it stops responding, then this component stops working.
 const LinkDisplay = () => {
   const { loading, error, data } = useQuery(GET_ALL_LINKS);
 
-  console.log(data);
+  // console.log(data);
+  // console.log(data.getAllLinks);
 
   return (
-    <div>
-      <p>Link Display </p>
-      {/* {data.getAllLinks.map((link) => (
+    <div className={styles.container}>
+      {data.getAllLinks.map((link) => (
         <p key={link.id}>
-          {link.url} {"=>"} {link.slug}
+          {link.url} {"   =>   "} {link.slug}
         </p>
-      ))} */}
+      ))}
     </div>
   );
 };
