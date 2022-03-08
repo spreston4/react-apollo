87bd554382e1fc9b39de83968fddb55a54e8755a
Sam Preston <sam.preston11@gmail.com>
2022-03-08 14:36:13 -0600

    Footer styling Footer no longer covers content as list of links grow.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import "./styles.css";
+// import "./styles.css";
+import styles from "./App.module.css";
 import LinkDisplay from "./Components/LinkDisplay/LinkDisplay";
 import ShortenLink from "./Components/ShortenLink/ShortenLink";
 import Header from "./Components/Header/Header";
@@ -6,10 +7,12 @@ import Footer from "./Components/Footer/Footer";
 
 export default function App() {
   return (
-    <div className="App">
-      <Header />
-      <ShortenLink />
-      {/* <Footer /> */}
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <Header />
+        <ShortenLink />
+      </div>
+      <Footer />
     </div>
   );
 }
